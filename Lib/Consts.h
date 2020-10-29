829f0e4c4aa5eea321dec88bd2ab6a36ecb72aa8
DavidPgl <panagiotopulos.d@gmail.com>
2020-10-30 00:34:13 +0100

    [Added textures]

--- Lib/Consts.h
+++ Lib/Consts.h
@@ -6,36 +6,36 @@
 struct Consts {
 public:
 
-    struct SCREEN {
-    public:
-        static const unsigned int WIDTH = 1920;
-        static const unsigned int HEIGHT = 1080;
-    };
+	struct SCREEN {
+	public:
+		static const unsigned int WIDTH = 1920;
+		static const unsigned int HEIGHT = 1080;
+	};
 
-    struct PATHS {
-    public:
-        inline static const char *VERTEX_SHADER = "Shaders/shader.vert";
-        inline static const char *FRAGMENT_SHADER = "Shaders/shader.frag";
-        inline static const char *DEPTH_MAP_VERT_SHADER = "Shaders/depthMap.vert";
-        inline static const char *DEPTH_MAP_FRAG_SHADER = "Shaders/depthMap.frag";
-        inline static const char* DEPTH_MAP_VISUAL_VERT = "Shaders/depthMapVisual.vert";
-        inline static const char* DEPTH_MAP_VISUAL_FRAG = "Shaders/depthMapVisual.frag";
-        inline static const char* SHADOW_VERT_SHADER = "Shaders/shadowShader.vert";
-        inline static const char* SHADOW_FRAG_SHADER = "Shaders/shadowShader.frag";
+	struct PATHS {
+	public:
+		inline static const char* VERTEX_SHADER = "Shaders/shader.vert";
+		inline static const char* FRAGMENT_SHADER = "Shaders/shader.frag";
+		inline static const char* DEPTH_MAP_VERT_SHADER = "Shaders/depthMap.vert";
+		inline static const char* DEPTH_MAP_FRAG_SHADER = "Shaders/depthMap.frag";
+		inline static const char* DEPTH_MAP_VISUAL_VERT = "Shaders/depthMapVisual.vert";
+		inline static const char* DEPTH_MAP_VISUAL_FRAG = "Shaders/depthMapVisual.frag";
+		inline static const char* SHADOW_VERT_SHADER = "Shaders/shadowShader.vert";
+		inline static const char* SHADOW_FRAG_SHADER = "Shaders/shadowShader.frag";
 
-        inline static const char *TEXTURE_CRATE = "Art/Textures/woodenCrate.jpg";
-        inline static const char *TEXTURE_BRICK = "Art/Textures/brickWall.jpg";
-        inline static const char *TEXTURE_GRID = "Art/Textures/texture_09.png";
-        inline static const char *TEXTURE_GRID_TEXT = "Art/Textures/texture_13.png";
-        inline static const char *TEXTURE_SKY = "Art/Textures/sky.jpg";
-        inline static const char *TEXTURE_RED = "Art/Textures/red.png";
-        inline static const char *TEXTURE_AWESOME = "Art/Textures/awesomeface.png";
-    };
+		inline static const char* TEXTURE_CRATE = "Art/Textures/woodenCrate.jpg";
+		inline static const char* TEXTURE_BRICK = "Art/Textures/brickWall.jpg";
+		inline static const char* TEXTURE_GRID = "Art/Textures/unityGrid.jpg";
+		inline static const char* TEXTURE_WHITE = "Art/Textures/white.jpg";
+		inline static const char* TEXTURE_AWESOME = "Art/Textures/awesomeface.png";
+	};
 
-    struct RENDERING{
-    public:
-        static const bool USE_WIREFRAME_MODE = false;
-    };
+	struct RENDERING {
+	public:
+		static const bool USE_WIREFRAME_MODE = false;
+		static const unsigned int SHADOW_WITH = 2048; 
+		static const unsigned int SHADOW_HEIGHT = 2048;
+	};
 };
 
 

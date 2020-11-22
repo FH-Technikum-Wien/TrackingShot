770df0f94111e6fc0bce7ebeaa9c5258e1e8f6ce
DavidPgl <panagiotopulos.d@gmail.com>
2020-11-22 13:46:53 +0100

    [Changed default materials]

--- Lib/Datatypes/Material.cpp
+++ Lib/Datatypes/Material.cpp
@@ -3,52 +3,68 @@
 #include "../Consts.h"
 #include <GLFW/glfw3.h>
 
-Material Material::ColorMat(glm::vec3 color)
-{
-	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_WHITE, GL_RGB);
-	return Material(&texture, color);
-}
-
-Material Material::GridMat(glm::vec3 color)
-{
-	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_GRID, GL_RGB);
-	return Material(&texture, color);
-}
-
-Material Material::WoodCrateMat(glm::vec3 color)
-{
-	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_CRATE, GL_RGB);
-	return Material(&texture, color);
-}
-
-Material Material::BrickMat(glm::vec3 color)
+Material Material::BrickMat()
 {
 	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_BRICK, GL_RGB);
-	return Material(&texture, color);
-}
-
-Material Material::MetalMat()
-{
-	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_WHITE, GL_RGB);
-	Material material = Material(&texture, glm::vec3(0.2f, 0.2f, 0.2f));
-	material.diffuseStrength = 0.2f;
-	material.specularStrength = 1.0f;
-	material.focus = 128;
+	unsigned int normalMap = Util::LoadTexture(Consts::PATHS::NORMAL_MAP_BRICK, GL_RGB);
+	Material material = Material(&texture, &normalMap, glm::vec3(1.0f));
+	material.ambientStrength = 1;
+	material.diffuseStrength = 1;
+	material.specularStrength = 0;
+	material.focus = 64;
 	return material;
 }
 
-Material::Material()
+Material Material::WoodMat()
 {
-	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_WHITE, GL_RGB);
-	this->texture = texture;
+	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_WOOD, GL_RGB);
+	unsigned int normalMap = Util::LoadTexture(Consts::PATHS::NORMAL_MAP_WOOD, GL_RGB);
+	Material material = Material(&texture, &normalMap, glm::vec3(1.0f));
+	material.ambientStrength = 1;
+	material.diffuseStrength = 1;
+	material.specularStrength = 0.7f;
+	material.focus = 8;
+	return material;
+}
+
+Material Material::RocksMat()
+{
+	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_ROCKS, GL_RGB);
+	unsigned int normalMap = Util::LoadTexture(Consts::PATHS::NORMAL_MAP_ROCKS, GL_RGB);
+	Material material = Material(&texture, &normalMap, glm::vec3(1.0f));
+	material.ambientStrength = 1;
+	material.diffuseStrength = 1;
+	material.specularStrength = 0.3f;
+	material.focus = 8;
+	return material;
+}
+
+Material Material::Brick2Mat()
+{
+	unsigned int texture = Util::LoadTexture(Consts::PATHS::TEXTURE_BRICK2, GL_RGB);
+	unsigned int normalMap = Util::LoadTexture(Consts::PATHS::NORMAL_MAP_BRICK2, GL_RGB);
+	Material material = Material(&texture, &normalMap, glm::vec3(1.0f));
+	material.ambientStrength = 1;
+	material.diffuseStrength = 1;
+	material.specularStrength = 0.2f;
+	material.focus = 32;
+	return material;
 }
 
 Material::Material(const unsigned int* texture)
 {
 	this->texture = *texture;
+	this->normalMap = Util::LoadTexture(Consts::PATHS::NORMAL_MAP_EMPTY, GL_RGB);
 }
 
 Material::Material(const unsigned int* texture, glm::vec3 color) : Material(texture)
 {
+	this->color = color;
+}
+
+Material::Material(const unsigned int* texture, const unsigned int* normalMap, glm::vec3 color)
+{
+	this->texture = *texture;
+	this->normalMap = *normalMap;
 	this->color = color;
 }
--- Lib/Datatypes/Material.h
+++ Lib/Datatypes/Material.h
@@ -5,20 +5,21 @@ class Material
 {
 public:
 	unsigned int texture = 0;
+	unsigned int normalMap = 0;
 	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
 	float ambientStrength = 1;
 	float diffuseStrength = 1;
 	float specularStrength = 0;
 	float focus = 64;
 	
-	static Material ColorMat(glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f));
-	static Material GridMat(glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f));
-	static Material WoodCrateMat(glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f));
-	static Material BrickMat(glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f));
-	static Material MetalMat();
+	static Material BrickMat();
+	static Material WoodMat();
+	static Material RocksMat();
+	static Material Brick2Mat();
 
-	Material();
+	Material() = default;
 	Material(const unsigned int* texture);
 	Material(const unsigned int* texture, glm::vec3 color);
+	Material(const unsigned int* texture, const unsigned int* normalMap, glm::vec3 color);
 };
 
